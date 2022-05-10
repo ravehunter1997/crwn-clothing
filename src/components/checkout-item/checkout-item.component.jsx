0f60a2ed924fe6703b02b69e1c80f9dc57b66450
Anuj Verma <anujhim97@gmail.com>
2022-05-11 00:23:56 +0530

    Y

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -16,11 +16,11 @@ export const CheckOutItem = ({ cartItem }) => {
       </div>
       <span className="name">{name}</span>
       <span className="quantity">
-        <div className="arrow" onClick={removeItemHandler}>
+        <div className="arrow" onClick={addItemHandler}>
           &#10094;
         </div>
         <span className="value">{quantity}</span>
-        <div className="arrow" onClick={addItemHandler}>
+        <div className="arrow" onClick={ removeItemHandler}>
           &#10095;
         </div>
       </span>
